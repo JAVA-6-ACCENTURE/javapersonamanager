9495f89dd46f33bd0e7757aa5f2fde7549099ed0
LainBal <aleponijs@gmail.com>
2020-06-20 14:48:04 +0300

    Person Manager task

---
+++ laineJavaPM/src/lv/laine/Main.java
@@ -0,0 +1,12 @@
+package lv.laine;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ laineJavaPM/src/lv/laine/PeopleManagerApplication.java
@@ -0,0 +1,32 @@
+package lv.laine;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+public class PeopleManagerApplication {
+
+    public static void main(String[] args) {
+//        LocalDate dob = LocalDate.of(1991, 4, 19);
+//        LocalDate date = LocalDate.now();
+//        Period diff = Period.between(dob, date);
+//        System.out.println(diff.getYears()+ "years" + diff.getMonths() + "months" + diff.getDays() + "days");
+        Person person = new Person("Laine", "Aleponija", "Balode", "f", LocalDate.of(1991,4,19));
+        System.out.println(person);
+
+        System.out.println("-------------");
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Str1");
+        System.out.println(arrayList);
+        arrayList.remove("Str1");
+        System.out.println(arrayList);
+
+        Person person1 = new Person("Karlis", "Edmunds", "Baumanis", "m", LocalDate.of(1991,4,19));
+        ArrayList<Person> arrayList1 = new ArrayList<>();
+        PeopleManager peopleManager = new PeopleManager(arrayList1);
+
+        peopleManager.addPerson(person1);
+        System.out.println(arrayList1);
+
+    }
+}
